ee7aae50d055325a0e723bb766c90b481fd660ad
valentynaLogosha <cristianyshka@gmail.com>
2023-05-13 19:15:34 +0300

    add style css and task 8 and 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,18 @@
+// Напиши скрипт, який змінює кольори фону елемента < body >
+//   через інлайн - стиль по кліку на button.change - color і виводить значення кольору в span.color.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function changeBackgroundColor() {
+  const color = getRandomHexColor();
+  document.body.style.background = color;
+  document.querySelector('.color').textContent = color;
+
+}
+const button = document.querySelector('.change-color');
+button.addEventListener('click', changeBackgroundColor);
+
